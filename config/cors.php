c4b718e9456774e6e0800203a9670189c3283231
Ronniel Sajol <sajolronniel28@gmail.com>
2026-05-02 01:06:21 +0800

    added frontend to cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', "https://hbd-neng-2026.vercel.app"],
 
     'allowed_origins_patterns' => [],
 
